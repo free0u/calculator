5419a6bf291b9e6f1dd2e00aa816fdc93890b2af
Anton Evdokimov <free0u@gmail.com>
2012-11-10 01:02:54 +0400

    Add first UI

--- src/ru/free0u/calculator/MainActivity.java
+++ src/ru/free0u/calculator/MainActivity.java
@@ -3,13 +3,21 @@ package ru.free0u.calculator;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	TextView tv;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        MathParser mp = new MathParser();
+        double res = mp.evaluate("2+2");
+        
+        tv = (TextView)findViewById(R.id.textView_out);
+        tv.setText(Double.toString(res));
     }
-    
 }
