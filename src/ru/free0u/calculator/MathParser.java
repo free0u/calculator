36d7bea3606ffa7879d5b864898372f1da8afe8d
Anton Evdokimov <free0u@gmail.com>
2012-11-10 01:40:25 +0400

    Add unit test project and some tests

--- src/ru/free0u/calculator/MathParser.java
+++ src/ru/free0u/calculator/MathParser.java
@@ -1,8 +1,14 @@
 package ru.free0u.calculator;
 
 public class MathParser {
-	MathParser() {
+	final double eps = 1e-5;
+	
+	public MathParser() {
 		
+	}
+	
+	public boolean equalDouble(double a, double b) {
+		return Math.abs(a - b) < eps;
 	}
 	
 	public double evaluate(String exp) {
