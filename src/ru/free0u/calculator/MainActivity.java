48eb0aea5319364dadbf241e83bea6a43e6ccda5
Anton Evdokimov <free0u@gmail.com>
2012-11-10 21:43:04 +0400

    Add click listener to button (improve UI)

--- src/ru/free0u/calculator/MainActivity.java
+++ src/ru/free0u/calculator/MainActivity.java
@@ -2,29 +2,54 @@ package ru.free0u.calculator;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
 	TextView tv;
+	EditText et;
+	Button bt;
+	
+	MathParser mp = null;
+	
+	private void setExpressionResult(String exp) {
+		String result;
+		try {
+        	double res = mp.evaluate(exp);
+        	result = Double.toString(res);
+        } catch (IllegalArgumentException e) {
+        	result = "Illegal expression";
+        }
+		tv.setText(result);
+	}
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        MathParser mp = new MathParser();
-        
-        String result;
-        try {
-        	double res = mp.evaluate("-+2");
-        	result = Double.toString(res);
-        } catch (IllegalArgumentException e) {
-        	result = "Illegal expression";
-        }
-        
         tv = (TextView)findViewById(R.id.textView_out);
-        tv.setText(result);
+        et = (EditText)findViewById(R.id.editText1);
+        bt = (Button)findViewById(R.id.button1);
+        bt.setOnClickListener(this);
+        
+        mp = new MathParser();
+        
+        setExpressionResult("(1)*((2))*(((3)))*((((4))))");
     }
+
+	public void onClick(View v) {
+		if (v.getId() == R.id.button1) {
+			Log.i("calc", "onClick button");
+			String exp = et.getText().toString();
+			Log.i("calc", "exp: " + exp);
+			setExpressionResult(exp);
+		}
+	}
 }
